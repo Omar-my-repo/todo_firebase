eec8d2c76eef4aa693e3eed1686823bb9b4ac6d0
Dsc-tasks <clinicawy@gmail.com>
2022-12-06 15:51:24 +0200

    + task list ui complete

--- lib/screens/tasklist_screen.dart
+++ lib/screens/tasklist_screen.dart
@@ -1,4 +1,5 @@
 import 'package:calendar_timeline/calendar_timeline.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:route_todoapp/style/color.dart';
 
@@ -31,6 +32,66 @@ class _TasksScreenState extends State<TasksScreen> {
           locale: 'en',
         ),
         SizedBox(height: 20),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 12,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 3,
+                      height: 80,
+                      color: primaryColor,
+                    ),
+                    SizedBox(width: 14),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'task Tilte',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline1!
+                                .copyWith(color: primaryColor, fontSize: 26),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'task desc',
+                            style: Theme.of(context)
+                                .textTheme
+                                .subtitle1!
+                                .copyWith(color: colorBlack, fontSize: 22),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 14),
+                    Container(
+                      width: 40,
+                      height: 35,
+                      decoration: BoxDecoration(
+                        color: primaryColor,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Icon(
+                        Icons.check,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        )
       ],
     );
   }
